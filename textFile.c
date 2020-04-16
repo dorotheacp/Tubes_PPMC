4f13f025ce668e2d2750403b9852ffd5032b378e
ameliamle <60213891+ameliamle@users.noreply.github.com>
2020-04-16 13:54:49 +0700

    Sudah diupdate

--- textFile.c
+++ textFile.c
@@ -1,11 +1,21 @@
-//INI MASIH MAU DISELESAIKAN
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+*  MODUL 8 – TUGAS BESAR
+*  Kelompok         : 4
+*  Hari/Tanggal     : Kamis, 16 April 2020
+*  Asisten	    : Devi Tara Avalokita (18316024)
+*  Nama File        : textFile.c
+*  Deskripsi        : 
+*  1. textFile untuk menginput kata dari file eksternal ke dalam array of words dan
+*     menghitung jumlah kata yang ada di dalam file eksternal
+*/
 
 #include <stdio.h>
 #include <string.h>
 
 //Input : namaFileEksternal.txt
-//Output : struct of array kata dan jumlah kata
+//Output : array of words dan jumlah kata
 
+//Struct of word
 typedef struct {
 	char c[100];
 } word;
@@ -14,48 +24,22 @@ typedef struct {
 void textFile (char new[100])
 {
 	FILE * document;
-	int i;
-	word kata[3000];
-	
-	document = fopen(new, "r");
-	if (document != NULL){
-		i = 0;
-		while (fgets(kata[i].c, sizeof kata[i].c, document) != NULL){
-			//Masih salah, ini ngambilnya per baris, nanti dulu
-			//printf("%s", kata[i].c);
-			//kata[i].c = kata;
-			i++;
-		}
-	}
-	fclose(document);
-}
-
-//Prosedur untuk menghitung jumlah kata pada file
-void hitungKata(char new[100]){
-	int kata, huruf;
-	FILE * document;
-	char ch;
+	word text[3000];
+	int kata = 0;
 	
 	document = fopen(new, "r");
 	
-	if (document == NULL){
-		kata = 0;
-	}
-	
-	else{
-		kata = 0;
-		while((ch = fgetc(document)) != EOF){
-			huruf++;
-			
-			if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0')
-				kata++;
-		}
-		if (huruf>0){
+	if (document != NULL)
+	{
+		while (!feof(document))
+		{
+			fscanf(document, "%s\n", text[kata].c);
+			//printf("%s\n", text[kata].c); (untuk testing)
 			kata++;
 		}
 	}
+	//printf("%d",kata); (untuk testing)
 	fclose(document);
-	
 }
 
 /* Tes fungsi
@@ -63,7 +47,6 @@ int main()
 {
 	char doc[100];
 	scanf("%s", doc);
-	hitungKata(doc);
-	printf("jumlah kata: %d", kata);
+	textFile(doc);
 }
 */
